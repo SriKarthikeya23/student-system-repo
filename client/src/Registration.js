f29a19f4b0674ea4c85311059a7da5baadc62b92
SriKarthikeya23 <66420092+SriKarthikeya23@users.noreply.github.com>
2022-12-06 16:36:53 +0530

    Update Registration.js

--- client/src/Registration.js
+++ client/src/Registration.js
@@ -20,7 +20,7 @@ function Registration() {
     
     const addStudentDetails = () => {
       window.location.reload();
-        Axios.post('http://localhost:3001/create', {
+        Axios.post('http://65.49.44.136:3003/create', {
             idno:idno,
             rfidno:rfidno,
             firstname:firstname,
